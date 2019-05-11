84e7de4642f5efa86f440e222ece960a5ab36830
virxs <garuskin.dima@gmail.com>
2019-05-12 02:35:32 +0300

    Refactor
    Оптимизация кода

--- Plugin/Command.php
+++ Plugin/Command.php
@@ -1,28 +1,21 @@
 <?php
 function Command($vk, $cmd){
-    if($cmd[0] == 'rview'){
+    if($cmd[0] == 'rview') {
         if($cmd[1]){
-            $dialog_role = array();
-            $user = Vk::User($vk->object->peer_id);
-            if($role_list = Role::Name($cmd[1])){
-            foreach ($role_list as $user_role){
-                foreach ($user as $id){
-                    if($id == $user_role) array_push($dialog_role, $id);
-                }
+            if(!$role = Role::Name($cmd[1])){
+                Vk::Message('Участников беседы с ролью <'.$cmd[1].'> не найдено', $vk->object->peer_id);
+                return true;
             }
+            foreach($user = Vk::User($vk->object->peer_id) as $k => $id){
+                if(!in_array($id, $role)) unset($user[$k]);
             }
-            if(count($dialog_role) > 0){
-                $message = "Роль ".$cmd[1]." найдена у:\n";
-                foreach(User::Names($dialog_role, 'gen') as $name){
-                    $message .= '['.$name->first_name.' '.$name->last_name."]\n";
-                }
+            $user = array_values($user);
+            $message = "Роль ".$cmd[1]." найдена у:\n";
+            foreach(User::Names($user, 'gen') as $name){
+                $message .= '['.$name->first_name.' '.$name->last_name."]\n";
+            }
                 Vk::Message($message, $vk->object->peer_id);
                 return true;
-            }
-            else{
-                Vk::Message('Участников беседы с ролью '.$cmd[1].' не найдено', $vk->object->peer_id);
-                return true;
-            }
         }
     }
     return false;
